0d9ac1f17c667cf043ae8fa00769f37f4b99b37f
Guilherme Cassel <guilherme.cassel2004@gmail.com>
2025-01-02 10:25:26 -0300

    added passwordencoder class

---
+++ FriendlyPasswordGenerator.Model/PasswordEncoder.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace FriendlyPasswordGenerator.Model;
+
+public static class PasswordEncoder
+{
+    private const int SymbolCode = 0;
+    private const int NumberCode = 1;
+    private const int WordCode = 2;
+
+    private static Task<string> TranslateCode(int code)
+    {
+        Task<string> result = code switch
+        {
+            SymbolCode => Randomizer.RandomSymbol(),
+            NumberCode => Randomizer.RandomNumber(),
+            WordCode => Randomizer.RandomWord(),
+            _ => throw new InvalidDataException($"The following pattern Index doesn't exist: {code}"),
+        };
+
+        return result;
+    }
+
+    public static async Task<string> TranslatePattern(List<int> pattern)
+    {
+        StringBuilder result = new();
+
+        foreach (var index in pattern)
+        {
+            string? translatedValue = await TranslateCode(index);
+            result.Append(translatedValue);
+        }
+
+        return result.ToString();
+    }
+}
