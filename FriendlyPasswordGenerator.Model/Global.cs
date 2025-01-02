da559824bec4f499648fccaac9a19cd3bdf022d4
Guilherme Cassel <guilherme.cassel2004@gmail.com>
2025-01-02 10:25:47 -0300

    added global class, for general task simplification

--- FriendlyPasswordGenerator.Model/Global.cs
+++ FriendlyPasswordGenerator.Model/Global.cs
@@ -5,17 +5,19 @@ namespace FriendlyPasswordGenerator.Model;
 
 public static class Global
 {
-    public static async Task<string> RunWithTimeout(Task<string> _task, int timeout)
+    public static async Task<T> RunWithTimeout<T>(Task<T> task, int timeout)
     {
         using var cts = new CancellationTokenSource();
 
         var delayTask = Task.Delay(timeout, cts.Token);
 
         // Cancel the task if it takes longer than the timeout
-        if (await Task.WhenAny(_task, delayTask) == delayTask)
+        if (await Task.WhenAny(task, delayTask) == delayTask)
         {
             cts.Cancel();
             throw new OperationCanceledException(cts.Token);
         }
+
+        return await task;
     }
 }
